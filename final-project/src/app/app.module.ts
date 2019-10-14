b3027253cd2686fb51c19bdb613cd4b0d02beefd
Vitalii Martyniak <46277409+VitaliyMartyniak@users.noreply.github.com>
2019-10-14 15:34:38 +0300

    branches and components created

---
+++ final-project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { UsersListComponent } from './users-list/users-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationComponent,
+    AdminPageComponent,
+    UsersListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
