7e845db1daa7598eef83d0c31486c469086d919e
Vitalii Martyniak <46277409+VitaliyMartyniak@users.noreply.github.com>
2019-10-14 15:45:31 +0300

    navigation created

--- final-project/src/app/app-routing.module.ts
+++ final-project/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegistrationComponent} from './registration/registration.component';
+import {UsersListComponent} from './users-list/users-list.component';
+import {AdminPageComponent} from './admin-page/admin-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'users-list', component: UsersListComponent},
+  {path: 'admin-page', component: AdminPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
