a0448ce62b733829fb3b35c81df3f69f2a445980
Vitalii Martyniak <46277409+VitaliyMartyniak@users.noreply.github.com>
2019-10-24 23:47:38 +0300

    login and logout from localstorage

--- final-project/src/app/app.component.ts
+++ final-project/src/app/app.component.ts
@@ -9,29 +9,45 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit, OnInit {
+  public adminPass = {login: 'admin', password: 'admin'};
   public userIsAdmin: boolean = false;
+  public darkThemeMode: boolean = false;
   modalReference: NgbModalRef;
   @ViewChild ('content' , {static: false}) content;
-  public darkThemeMode: boolean = false;
   constructor(private modalService: NgbModal, public userTableService: UserTableService, public router: Router) {}
 
+  ngOnInit() {
+    const user = JSON.parse(localStorage.getItem('admin'));
+    if (user === null) {
+      return;
+    } else if (user.login === this.adminPass.login && user.password === this.adminPass.password) {
+      this.userIsAdmin = true;
+    }
+
+    this.userTableService.darkThemeSubject$.subscribe(darkThemeStatus => {
+      this.darkThemeMode = darkThemeStatus;
+    });
+  }
+
   ngAfterViewInit() {
-    this.modalReference = this.modalService.open(this.content, { centered: true });
+    if (!this.userIsAdmin) {
+      this.modalReference = this.modalService.open(this.content, {centered: true});
+    }
   }
 
   checkAdmin(login, password) {
-    if (login === 'admin' && password === 'admin') {
+    if (login === this.adminPass.login && password === this.adminPass.password) {
       this.userIsAdmin = true;
+      localStorage.setItem('admin', JSON.stringify(this.adminPass));
       this.modalReference.close(this.content);
     } else {
       alert('Login or password is incorrect!');
     }
   }
 
-  ngOnInit() {
-    this.userTableService.darkThemeSubject$.subscribe(darkThemeStatus => {
-      this.darkThemeMode = darkThemeStatus;
-    });
+  clearLocalStorage() {
+    localStorage.clear();
+    window.location.reload();
   }
 
 }
